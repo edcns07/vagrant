512ef27b77220473ab281b0ab393a954c8249ab1
Ed <ed.sangalang@gmail.com>
2021-06-08 19:27:33 -0700

    initial-commit

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Update OS w any paches
+yum update -y --exclude=kernel
+#Tools
+yum install -y nano git unzip screen
+#Apache
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant/www/html
+service httpd start
+
+#PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+#MySQL
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+#Download  Starter Content
+service httpd restart
+
+
+
