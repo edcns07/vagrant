c8f6d0fb919fdcc1f32622e06fd7070fb5e2d597
Ed <ed.sangalang@gmail.com>
2021-06-11 22:13:34 -0700

    fix-sh

--- scripts/centos7.9-common.sh
+++ scripts/centos7.9-common.sh
@@ -2,6 +2,6 @@
 # Update OS w any paches
 #su # need to be root to run the updates
 #yum update -y --exclude=kernel
-sudo yum update -y --exclude=kernel
+sudo yum update -y
 #Tools
 sudo yum install -y nano git unzip screen wget nc telnet
